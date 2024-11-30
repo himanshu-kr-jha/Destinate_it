ddb36e9ed1a66d244e73f14b2636d0ebb2ece372
thegentledon <thegentledon004@gmail.com>
2024-11-30 16:59:50 +0530

    added feature of add from map and styling

--- middleware.js
+++ middleware.js
@@ -22,8 +22,8 @@ module.exports.saveRedirectUrl=(req,res,next)=>{
 module.exports.isOwner=async (req,res,next)=>{
     let {id}=req.params;
     let listing= await Listing.findById(id);
-    console.log("owner"+listing.owner);
-    console.log("locals"+res.locals.currUser._id);
+    // console.log("owner"+listing.owner);
+    // console.log("locals"+res.locals.currUser._id);
     if(!listing.owner.equals(res.locals.currUser._id)){
         req.flash("error","You do not have permission.");
         return res.redirect(`/listing/${id}`);
